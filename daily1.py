e324a10b8b0e564bee9c01ac858a2de0028a2aa5
jai711 <51733850+jai711@users.noreply.github.com>
2019-06-14 11:41:52 +0530

    14/06/19

---
+++ daily1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 12:55:08 2019
+
+@author: jai
+"""
+
+x = 10
+y = 3
+print("10 divided by 3 is", x/y)
+print("remainder after 10 divided by 3 is", x%y)
